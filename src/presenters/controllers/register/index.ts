b9be86c6a9d61d236398757a3365245968ec0b03
Thiago-duart <td1405150+github@gmail.com>
2024-05-07 11:44:59 -0300

    feat: must call encrypterAdapter with password from body

--- src/presenters/controllers/register/index.ts
+++ src/presenters/controllers/register/index.ts
@@ -1,2 +1,4 @@
 export { IUserMethods } from "../../../domain/usecase/user-usecase";
 export { IControllers, IHttpResponse } from "../../interfaces/controller";
+export { EncrypterAdapter } from "../../../encrypt/encrypter-adapter";
+export { IEncrypter } from "../../interfaces/encrypter";
--- src/presenters/controllers/register/register-controller.ts
+++ src/presenters/controllers/register/register-controller.ts
@@ -1,9 +1,24 @@
-import { IUserMethods, IControllers, IHttpResponse } from "./";
+import {
+  IUserMethods,
+  IControllers,
+  IHttpResponse,
+  EncrypterAdapter,
+  IEncrypter,
+} from "./";
 
 export class RegisterController implements IControllers {
-  constructor(private readonly userMethods: IUserMethods) {}
+  constructor(
+    private readonly userMethods: IUserMethods,
+    private readonly EncrypterAdapter: IEncrypter
+  ) {}
   async handle(request: any): Promise<IHttpResponse> {
-    const user = await this.userMethods.add(request.body);
+    const hashPassword = await this.EncrypterAdapter.encrypt(
+      request.body.password
+    );
+    const user = await this.userMethods.add({
+      ...request.body,
+      password: hashPassword,
+    });
     return { statusCode: 200, body: {} };
   }
 }
