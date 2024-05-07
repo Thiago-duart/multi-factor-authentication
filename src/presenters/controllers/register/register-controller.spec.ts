1f0b2b8190c748f089eef623aca2086b2f217df8
Thiago-duart <td1405150+github@gmail.com>
2024-05-07 11:45:15 -0300

    test: must call encrypterAdapter with password from body

--- src/presenters/controllers/register/register-controller.spec.ts
+++ src/presenters/controllers/register/register-controller.spec.ts
@@ -1,20 +1,29 @@
-import { IUserMethods } from "./";
+import { IUserMethods, IEncrypter } from "./";
 import { RegisterController } from "./register-controller";
 
 describe("register", () => {
   const makeSut = () => {
     class UserMethodsStub implements IUserMethods {
-      add(userData: any): any {}
+      add(userData: any): any {
+        return true;
+      }
     }
-    const userMethods = new UserMethodsStub();
-    const sut = new RegisterController(userMethods);
+    class EncrypterStub implements IEncrypter {
+      async encrypt(password: string): Promise<string> {
+        return password;
+      }
+    }
+    const userMethodsStub = new UserMethodsStub();
+    const encrypterStub = new EncrypterStub();
+    const sut = new RegisterController(userMethodsStub, encrypterStub);
     return {
-      userMethods,
+      userMethodsStub,
       sut,
+      encrypterStub,
     };
   };
-  test("must call user methods with data from the request body", () => {
-    const { userMethods, sut } = makeSut();
+  test("must call user methods with data from the request body", async () => {
+    const { userMethodsStub, sut } = makeSut();
     const user = {
       body: {
         name: "valid-name",
@@ -22,8 +31,21 @@ describe("register", () => {
         password: "valid-password",
       },
     };
-    const userMethodsSpy = jest.spyOn(userMethods, "add");
-    sut.handle(user);
+    const userMethodsSpy = jest.spyOn(userMethodsStub, "add");
+    await sut.handle(user);
     expect(userMethodsSpy).toHaveBeenCalledWith(user.body);
+  });
+  test("must call encrypterAdapter with password from body ", () => {
+    const { encrypterStub, sut } = makeSut();
+    const user = {
+      body: {
+        name: "valid-name",
+        email: "valid-email",
+        password: "valid-password",
+      },
+    };
+    const encryptSpy = jest.spyOn(encrypterStub, "encrypt");
+    sut.handle(user);
+    expect(encryptSpy).toHaveBeenCalledWith(user.body.password);
   });
 });
