37068be17e7cf79d08f175fc54926502934425f3
Thiago-duart <td1405150+github@gmail.com>
2024-05-09 11:16:13 -0300

    feat: create first migration

---
+++ src/infra/postgres/migrations/1715256881758-user.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class User1715256881758 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "name",
+            type: "varchar",
+            length: "50",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            length: "50",
+          },
+          {
+            name: "password",
+            type: "varchar",
+            length: "120",
+          },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updatedAt",
+            type: "timestamp",
+            isNullable: true,
+          },
+        ],
+      }),
+      true
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
