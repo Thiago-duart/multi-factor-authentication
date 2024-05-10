f05fcda8936d8ad1e55798bde0c375732b84bbe7
Thiago-duart <td1405150+github@gmail.com>
2024-05-10 14:33:22 -0300

    refactor: rename environment variables

--- src/infra/postgres/helper/db-production-connection.ts
+++ src/infra/postgres/helper/db-production-connection.ts
@@ -1,14 +1,15 @@
-import { DataSource } from "typeorm";
+import "reflect-metadata";
 import "dotenv/config";
+import { DataSource } from "typeorm";
 import { User } from "../entity/user.entity";
 
 export const postgresProduction = new DataSource({
   type: "postgres",
-  host: process.env.host,
-  port: +process.env.port,
-  username: process.env.username,
-  password: process.env.password,
-  database: process.env.database,
+  host: process.env.prod_host,
+  port: +process.env.prod_port,
+  username: process.env.prod_username,
+  password: process.env.prod_password,
+  database: process.env.prod_database,
   entities: [User],
   migrations: ["./src/infra/postgres/migration/*.ts"],
   synchronize: false,
--- src/infra/postgres/helper/db-test-connection.ts
+++ src/infra/postgres/helper/db-test-connection.ts
@@ -5,11 +5,11 @@ import { User } from "../entity/user.entity";
 
 export const postgresTest = new DataSource({
   type: "postgres",
-  host: process.env.test_host,
-  port: +process.env.test_port,
-  username: process.env.test_username,
-  password: process.env.test_password,
-  database: process.env.test_database,
+  host: process.env.dev_host,
+  port: +process.env.dev_port,
+  username: process.env.dev_username,
+  password: process.env.dev_password,
+  database: process.env.dev_database,
   entities: [User],
   migrations: ["./src/infra/postgres/migrations/*.ts"],
   synchronize: false,
