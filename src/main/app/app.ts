6994e30a36c0fd45d5c73b0054d6aa7021821bf7
Thiago-duart <td1405150+github@gmail.com>
2024-05-16 11:14:46 -0300

    feat: must be able to connect to the database and handle json

--- src/main/app/app.ts
+++ src/main/app/app.ts
@@ -1,7 +1,9 @@
+import "reflect-metadata";
 import express, { Application, json } from "express";
-
+import { registerRouter } from "../router/register.router";
+import cors from "cors";
 const app: Application = express();
-
-app.use(json);
+app.use(json(), cors());
+app.use("/auth", registerRouter);
 
 export default app;
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,26 +1,12 @@
 import "reflect-metadata";
-import { postgresProduction } from "../../src/infra/postgres/helper/db-production-connection";
-import { postgresTest } from "../../src/infra/postgres/helper/db-test-connection";
 import app from "./app/app";
+import { AppDataSource } from "../infra/postgres/database/data-soucer";
 
-const port = +process.env.PORT_APP || 3000;
-
-if (process.env.NODE_ENV === "dev") {
-  postgresTest
-    .initialize()
-    .then(() => {
-      app.listen(port, () => {
-        console.log(`app start db-test on port ${port}`);
-      });
-    })
-    .catch((err) => console.log(err));
-} else {
-  postgresProduction
-    .initialize()
-    .then(() => {
-      app.listen(port, () => {
-        console.log(`app start db-test on port ${port}`);
-      });
-    })
-    .catch((err) => console.log(err));
-}
+AppDataSource.initialize()
+  .then(async () => {
+    const PORT: number = +process.env.PORT! || 3000;
+    app.listen(PORT, async (): Promise<void> => {
+      console.log("app running on port ", PORT);
+    });
+  })
+  .catch((err) => console.error(err));
